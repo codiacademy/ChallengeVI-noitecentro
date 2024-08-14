1e65778b00318040c057419be87dfed12985db73
Fernando Mello <newodnanref@gamil.com>
2024-08-14 17:20:22 -0300

    Magic-Codi

--- frontend/react-page-frontend/src/pages/home/home.jsx
+++ frontend/react-page-frontend/src/pages/home/home.jsx
@@ -11,60 +11,39 @@ export function Home(){
     )
 }
 
+import React from 'react';
 
-<div className="header" id="header"></div>
-<div className="logo_header">
-    <img src="" alt="">
-</div>
-<div className="navigation_header">
-    <a href="#">Sobre nós</a>
-    <a href="#"></a>
-    <a href="#">Serviços</a>
-    <a href="#">Trabalhe conosco</a>
-    <a href="#">Contato</a>
-</div>
-<div class="idps">
-    <h1>INOVAÇÃO DIGITAL E PERSONALIZADA PARA O SEU SUCESSO</h1>
-    <p>
-        Não é apenas sobre desenvolvimento de software, é sobre envolvimento! Buscamos entender suas necessidades nos mínimos detalhes para oferecer soluções que realmente façam sentido, desde o primeiro contato até a entrega final.
-    </p>
-    <input type="button" value="acesse"> 
-</div>
-<div class="sn">
-    <h1>Sobre nós</h1>
-    <img src="aula4/nos.jpeg" alt="">
-    
-    <p>Somos uma empresa Software que nasceu para ir além de “apenas  entregar um projeto”. Criamos uma estrutura operacional baseada em organização, agilidade, transparência, colaboração e metodologias ágeis bem definidas.
-        ‍
-        ‍Não é sobre desenvolvimento, é sobre o real envolvimento. É sobre idealizar e lapidar, não “apenas entregar”. É sobre contribuir diretamente para que o potencial máximo de cada projeto seja explorado!</p>
+function MyComponent() {
+  return (
+    <>
+      <div className="idps">
+        <h1>INOVAÇÃO DIGITAL E PERSONALIZADA PARA O SEU SUCESSO</h1>
         <p>
-            É com grande satisfação que nos destacamos como uma referência incontestável no mercado de tecnologia. Desde a sua fundação, a Magic, tem continuamente superado expectativas e estabelecido novos padrões de excelência e inovação.
+          Não é apenas sobre desenvolvimento de software, é sobre envolvimento! Buscamos entender suas necessidades nos mínimos detalhes para oferecer soluções que realmente façam sentido, desde o primeiro contato até a entrega final.
+        </p>
+        <input type="button" value="acesse" />
+      </div>
+
+      <div className="sn">
+        <h1>Sobre nós</h1>
+        <img src="aula4/nos.jpeg" alt="" />
+        <p>
+          Somos uma empresa de Software que nasceu para ir além de “apenas entregar um projeto”. Criamos uma estrutura operacional baseada em organização, agilidade, transparência, colaboração e metodologias ágeis bem definidas. ‍Não é sobre desenvolvimento, é sobre o real envolvimento. É sobre idealizar e lapidar, não “apenas entregar”. É sobre contribuir diretamente para que o potencial máximo de cada projeto seja explorado!
         </p>
         <p>
-            Com uma visão estratégica e um compromisso inabalável com a qualidade, a magic não apenas conquistou uma posição de liderança, mas também influenciou positivamente o setor de desenvolvimento de software. Seus produtos e serviços excepcionais não apenas atendem às necessidades dos clientes, mas também definem tendências e inspiram concorrentes.
+          É com grande satisfação que nos destacamos como uma referência incontestável no mercado de tecnologia. Desde a sua fundação, a Magic, tem continuamente superado expectativas e estabelecido novos padrões de excelência e inovação.
         </p>
+        <p>
+          Com uma visão estratégica e um compromisso inabalável com a qualidade, a Magic não apenas conquistou uma posição de liderança, mas também influenciou positivamente o setor de desenvolvimento de software. Seus produtos e serviços excepcionais não apenas atendem às necessidades dos clientes, mas também definem tendências e inspiram concorrentes.
+        </p>
+      </div>
+    </>
+  );
+}
 
-</div>
-<div class="tu">
-    <h1>Tecnologias que utilizamos</h1>
-    <p>Trabalhamos com as tecnologias mais atuais do mercado, considerando as necessidades específicas de cada software desenvolvido.</p>
-    <img src="aula4/1.png" alt="">
-    <img src="aula4/2.png" alt="">
-    <img src="aula4/3.png" alt="">
-    <img src="aula4/4.png" alt="">
-    <img src="aula4/5.png" alt="">
-    <img src="aula4/6.png" alt="">
-    <img src="aula4/7.png" alt="">
-    <img src="aula4/8.png" alt="">
-    <img src="aula4/9.png" alt="">
-    <img src="aula4/10.png" alt="">
-    <img src="aula4/11.png" alt="">
-    <img src="aula4/12.png" alt="">
-    <img src="aula4/13.png" alt="">
-    <img src="aula4/14.png" alt="">
-    <img src="aula4/15.png" alt="">
-
-</div>
+export default MyComponent;
 
 
-</div>
+
+
+
--- frontend/react-page-frontend/src/pages/sobre/sobre.jsx
+++ frontend/react-page-frontend/src/pages/sobre/sobre.jsx
@@ -5,3 +5,25 @@ export function Sobre() {
         </div>
     )
 }
+
+
+<div class="tu">
+    <h1>Tecnologias que utilizamos</h1>
+    <p>Trabalhamos com as tecnologias mais atuais do mercado, considerando as necessidades específicas de cada software desenvolvido.</p>
+    <img src="aula4/1.png" alt="">
+    <img src="aula4/2.png" alt="">
+    <img src="aula4/3.png" alt="">
+    <img src="aula4/4.png" alt="">
+    <img src="aula4/5.png" alt="">
+    <img src="aula4/6.png" alt="">
+    <img src="aula4/7.png" alt="">
+    <img src="aula4/8.png" alt="">
+    <img src="aula4/9.png" alt="">
+    <img src="aula4/10.png" alt="">
+    <img src="aula4/11.png" alt="">
+    <img src="aula4/12.png" alt="">
+    <img src="aula4/13.png" alt="">
+    <img src="aula4/14.png" alt="">
+    <img src="aula4/15.png" alt="">
+
+</div>
